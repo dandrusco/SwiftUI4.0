e3f73b5e2fb0b8ff78412bfa96f6d97b3b126bd1
dandrusco <41076761+dandrusco@users.noreply.github.com>
2023-11-03 14:47:56 -0300

    Mi primer commits desde Xcode

---
+++ CursoSwiftUI4/CursoSwiftUI4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CursoSwiftUI4
+//
+//  Created by Danilo Andrusco on 03-11-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hola Mundo")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
